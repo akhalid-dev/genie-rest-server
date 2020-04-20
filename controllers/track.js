09dbcdc4cc766f34e0336e94ff1d1f91b4d18812
akhalid7 <akhal022@uottawa.ca>
2020-04-20 17:59:55 -0400

    Tested

--- controllers/track.js
+++ controllers/track.js
@@ -2,8 +2,10 @@ const amzparser = require('../auxiliary/amzparser');
 
 exports.postItem = (req, res, next) => {
     const item = req.body.item;
+    console.log(item);
     checkItem(item)
     .then(details => {
+        console.log(details);
         res.json(details);
     })
     .catch(err => console.log(err));
